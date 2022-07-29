1ff7554b68d8f65bf71d30469baa01cba391d314
Paul Erwich <perwich@outlook.com>
2022-07-29 18:58:31 +0100

    Started making template

---
+++ Project/src/Game.cpp
@@ -0,0 +1,35 @@
+//
+// Created by MLG Erwich on 29/07/2022.
+//
+
+#include "Game.h"
+
+Game::Game(sf::RenderWindow& game_window) : window(game_window)
+{
+
+}
+
+Game::~Game()
+{
+
+}
+
+bool Game::init()
+{
+  return true;
+}
+
+void Game::input(sf::Event event)
+{
+
+}
+
+void Game::update(float dt)
+{
+
+}
+
+void Game::render()
+{
+
+}
---
+++ Project/src/Game.h
@@ -0,0 +1,27 @@
+//
+// Created by MLG Erwich on 29/07/2022.
+//
+
+#ifndef SFMLTEMPLATE_GAME_H
+#define SFMLTEMPLATE_GAME_H
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+
+class Game
+{
+ public:
+  Game(sf::RenderWindow& window);
+  ~Game();
+  bool init();
+  void input(sf::Event event);
+  void update(float dt);
+  void render();
+
+ private:
+  sf::RenderWindow& window;
+};
+
+
+#endif //SFMLTEMPLATE_GAME_H
