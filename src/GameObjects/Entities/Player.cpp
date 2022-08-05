1063bf19b92f663a1dc0753a41b11c02f0bda88a
scott <scott2.knapp@live.uwe.ac.uk>
2022-08-05 21:10:30 +0100

    added player animator

--- src/GameObjects/Entities/Player.cpp
+++ src/GameObjects/Entities/Player.cpp
@@ -1,7 +1,3 @@
-//
-// Created by MLG Erwich on 05/08/2022.
-//
-
 #include "Player.h"
 Player::Player(sf::RenderWindow& game_window) : Entity(game_window)
 {
@@ -10,10 +6,13 @@ Player::Player(sf::RenderWindow& game_window) : Entity(game_window)
 
 bool Player::init()
 {
-  if (!texture->loadFromFile("Data/Data/images/manclock.png"))
+  if (!texture->loadFromFile("Data/Data/images/playerAnimation.png"))
   {
     return false;
   }
+
+  //set anim state
+  setPlayerAnimation();
 
   sprite->setTexture(*texture);
 
@@ -46,11 +45,13 @@ void Player::keyPressed(sf::Event event)
   if (event.key.code == sf::Keyboard::D)
   {
     movement.x = 1.5;
+    current_animation = AnimationState::RUNNING;
   }
 
   if (event.key.code == sf::Keyboard::A)
   {
     movement.x = -1.5;
+    current_animation = AnimationState::RUNNING;
   }
 
   if (event.key.code == sf::Keyboard::Space && on_ground)
@@ -65,16 +66,21 @@ void Player::keyReleased(sf::Event event)
   if (event.key.code == sf::Keyboard::D)
   {
     movement.x = 0;
+    current_animation = AnimationState::IDLE;
   }
 
   if (event.key.code == sf::Keyboard::A)
   {
     movement.x = 0;
+    current_animation = AnimationState::IDLE;
   }
 }
 
 void Player::update(float dt)
 {
+  //updates anim state
+  playerAnimatorUpdate(dt);
+
   movement.y += 5 * dt;
 
   if (on_ground)
@@ -87,11 +93,13 @@ void Player::update(float dt)
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
     {
       movement.x = 1.5;
+      current_animation = AnimationState::RUNNING;
     }
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
     {
       movement.x = -1.5;
+      current_animation = AnimationState::RUNNING;
     }
   }
 
@@ -108,4 +116,111 @@ void Player::update(float dt)
 
   setMin();
   calculateMax();
+}
+
+void Player::setPlayerAnimation()
+{
+  // player animation
+  if (current_animation == AnimationState::IDLE)
+  {
+    player_texture_rectangle = sf::IntRect(0, 0, 32, 32);
+    sprite->setTextureRect(player_texture_rectangle);
+    std::cout<<"idle"<<std::endl;
+  }
+
+//  else if (current_animation == AnimationState::JUMPING)
+//  {
+//    player_texture_rectangle = sf::IntRect(0, 80, 49, 40);
+//    sprite->setTextureRect(player_texture_rectangle);
+//  }
+
+  else if (current_animation == AnimationState::RUNNING)
+  {
+    player_texture_rectangle = sf::IntRect(0, 0, 32, 32);
+    sprite->setTextureRect(player_texture_rectangle);
+  }
+}
+
+void Player::playerAnimatorUpdate(float dt)
+{
+  footstep_timer += 1.0f * dt;
+  //jump_timer += 0.7f * dt;
+
+  // player animation state
+  if (current_animation == AnimationState::RUNNING)
+  {
+    if (footstep_timer > 1.2f)
+    {
+      //player_footsteps.play();
+      footstep_timer = 0;
+    }
+    playerAnimator(
+      0.05f, current_animation, player_texture_rectangle, 160, 32);
+  }
+  else if (current_animation == AnimationState::JUMPING)
+  {
+    /*playerAnimator(
+      1.2f * dt, current_animation, player_texture_rectangle, 980, 49);*/
+  }
+  else if (current_animation == AnimationState::IDLE)
+  {
+    playerAnimator(
+      0.03f * dt, current_animation, player_texture_rectangle, 32, 32);
+  }
+  if (movement.x == 0)
+  {
+    current_animation = AnimationState::IDLE;
+  }
+  if (!on_ground)
+  {
+    current_animation = AnimationState::JUMPING;
+  }
+}
+
+
+void Player::playerAnimator(
+  float update_animation, AnimationState &current, sf::IntRect& player_texture_rectangles,
+  int max_size, int sprite_size)
+{
+  if (clock_animation_texture.getElapsedTime().asSeconds() >= update_animation)
+  {
+    if (current_animation == AnimationState::RUNNING)
+    {
+      player_texture_rectangle.left += sprite_size;
+      player_texture_rectangle.width  = 32;
+      player_texture_rectangle.top    = 0.f;
+      player_texture_rectangle.height = 32;
+      if (player_texture_rectangle.left >= max_size)
+      {
+        player_texture_rectangle.left = 0;
+      }
+    }
+
+   /* else if (current_animation == AnimationState::JUMPING)
+    {
+      player_texture_rectangle.left += sprite_size;
+      player_texture_rectangle.width  = 49;
+      player_texture_rectangle.top    = 80.f;
+      player_texture_rectangle.height = 40;
+      if (player_texture_rectangle.left >= max_size)
+      {
+        player_texture_rectangle.left = 0;
+      }
+    }
+*/
+    else if (current_animation == AnimationState::IDLE)
+    {
+      player_texture_rectangle.left += sprite_size;
+      player_texture_rectangle.width  = 32;
+      player_texture_rectangle.top    = 0;
+      player_texture_rectangle.height = 32;
+      if (player_texture_rectangle.left >= max_size)
+      {
+        player_texture_rectangle.left = 0;
+      }
+    }
+
+    clock_animation_texture.restart();
+    sprite->setTextureRect(player_texture_rectangle);
+  }
 }
--- src/GameObjects/Entities/Player.h
+++ src/GameObjects/Entities/Player.h
@@ -1,10 +1,7 @@
-//
-// Created by MLG Erwich on 05/08/2022.
-//
-
 #ifndef SFMLTEMPLATE_PLAYER_H
 #define SFMLTEMPLATE_PLAYER_H
 
+#include "../../GameStates/AnimationState.h"
 #include "Entity.h"
 
 class Player : public Entity
@@ -21,10 +18,21 @@ class Player : public Entity
   void keyReleased(sf::Event event);
 
   void update(float dt) override;
+  void setPlayerAnimation();
+  void playerAnimatorUpdate(float dt);
+  void playerAnimator(
+    float update_animation, AnimationState& current,
+    sf::IntRect& player_texture_rectangle, int max_size, int sprite_size);
 
  private:
   bool on_ground;
   const int SPEED = 100;
+  float footstep_timer = 0;
+
+  sf::IntRect player_texture_rectangle;
+  sf::Clock clock_animation_texture;
+  AnimationState current_animation = AnimationState::IDLE;
+
 };
 
 #endif // SFMLTEMPLATE_PLAYER_H
